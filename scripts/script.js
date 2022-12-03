81bb4959ef9245a0b8c6ce07d6610cf9c8741702
Daniel Lazzaroni <dlazzaroni@my.bcit.ca>
2022-12-02 20:40:55 -0800

    added text toggling for dark theme button

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 function clickHandler(event) {
     console.log("clicked", event.target.textContent)
-    if (event.target.textContent === 'Dark Theme')// background color button is pressed
+    if ((event.target.textContent === 'Dark Theme') || (event.target.textContent === 'Light Theme'))
     {
         changeBGColor()
     }
@@ -11,6 +11,19 @@ function changeBGColor(){
     document.body.classList.toggle("darkBody");
     var element = document.getElementById("aside");
     element.classList.toggle("darkSidebar");
+    
+
+}
+
+const btn = document.getElementById('button2');
+
+function changeText() {
+    if (btn.textContent === 'Dark Theme') {
+      btn.textContent = 'Light Theme';
+    } else {
+      btn.textContent = 'Dark Theme';
+    }
 }
 
 document.body.addEventListener("click", clickHandler)
+btn.addEventListener('click', changeText)
